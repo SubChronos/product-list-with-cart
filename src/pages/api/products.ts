56fad3cf2025f85367d01d92737f88f27e5a7e63
SubChronos <spainscorpionmola@gmail.com>
2026-04-01 18:49:48 +0200

    Main app with astro

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,11 +1,18 @@
 import productsJSON from "../../data/data.json"
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async ({ request }) => {
+export const GET: APIRoute = async ({ url }) => {
+  const query = url.searchParams.get('q') || '';
   const allProducts = productsJSON; //Guarda el json
+  console.log("query: ", query)
+  console.log("FULL URL:", url.toString());
+  
+  const filtredProduct = query ? allProducts.filter(product => 
+        product.name === query
+      ) : allProducts; // Si no hay búsqueda, devolvemos todos
 
   // Devolvemos la respuesta en formato JSON
-  return new Response(JSON.stringify(allProducts), {
+  return new Response(JSON.stringify(filtredProduct), {
     status: 200,
     headers: {
       'Content-Type': 'application/json'
